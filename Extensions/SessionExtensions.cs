4213d2aa7104f91a819bc8e72a1a606e2e9ffee2
bigboi00 <leearthhur02@gmail.com>
2024-08-04 07:50:30 +0800

    Final Version

---
+++ Extensions/SessionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace INFT3050.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static void SetObjectAsJsonWithExpiry(this ISession session, string key, object value, TimeSpan expiry)
+        {
+            var expiringObject = new ExpiringObject
+            {
+                Value = value,
+                ExpiryDate = DateTime.UtcNow.Add(expiry)
+            };
+
+            session.SetString(key, JsonConvert.SerializeObject(expiringObject));
+        }
+
+        public static T GetObjectFromJsonWithExpiry<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            if (value == null)
+            {
+                return default(T);
+            }
+
+            var expiringObject = JsonConvert.DeserializeObject<ExpiringObject>(value);
+            if (DateTime.UtcNow > expiringObject.ExpiryDate)
+            {
+                session.Remove(key);
+                return default(T);
+            }
+
+            return JsonConvert.DeserializeObject<T>(expiringObject.Value.ToString());
+        }
+
+        private class ExpiringObject
+        {
+            public object Value { get; set; }
+            public DateTime ExpiryDate { get; set; }
+        }
+    }
+}
